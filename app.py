821f389804167929b0758a2256b604bf4a3f5c36
Rodrigoinzaurralde <rodrigoinzaurralde97@gmail.com>
2025-09-01 21:29:25 -0300

    Primer commit api-ip

---
+++ app.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API Flask corriendo en Render ✅"
+
+@app.route("/mi-ip")
+def mi_ip():
+    url = "http://ip-api.com/json/?fields=61439"
+    try:
+        r = requests.get(url, timeout=5)
+        data = r.json()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
