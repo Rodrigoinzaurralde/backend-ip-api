a80b0803aa7108a85c2e00f840cd59b661bd1153
Rodrigoinzaurralde <rodrigoinzaurralde97@gmail.com>
2025-09-01 21:53:53 -0300

    CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -11,7 +13,8 @@ def home():
 
 @app.route("/mi-ip")
 def mi_ip():
-    url = "http://ip-api.com/json/?fields=61439"
+    ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+    url = f"http://ip-api.com/json/{ip}?fields=61439"
     try:
         r = requests.get(url, timeout=5)
         data = r.json()
